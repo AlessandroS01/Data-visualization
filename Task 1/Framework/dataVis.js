008e10c6aa59e993c91530e0355963c2c3592055
AlessandroS01 <seghini53@gmail.com>
2025-06-02 12:22:05 +0200

    Definition color list

--- Task 1/Framework/dataVis.js
+++ Task 1/Framework/dataVis.js
@@ -30,6 +30,10 @@ let scatter, radar, dataTable;
 
 // Add additional variables
 let selectedItems = [];
+const colorList = [
+    "#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00",
+    "#ffff33", "#a65628", "#f781bf", "#999999", "#66c2a5"
+];
 
 
 function init() {
