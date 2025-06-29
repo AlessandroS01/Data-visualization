0c4ae3bd2ab8005a1ea04b954b4d70ca67fc766f
Yongyeon <smn47vrs7@proton.me>
2025-06-29 22:39:25 +0200

    Added hovering

--- Task 2/Framework/scatterplot.js
+++ Task 2/Framework/scatterplot.js
@@ -9,15 +9,17 @@ let svg,
     originalX, originalY,
     fullData,
     xAccessor, yAccessor, sizeAccessor,
-    brush;
+    brush,
+    isBrushing = false; // NEW: State flag to track brushing/zooming action
+
+// --- 3. SCATTERPLOT INITIALIZATION AND UPDATES ---
 
 /**
  * Initializes the dashboard scatterplot.
  */
 function initDashboardScatterplot(config) {
-    // ... (This function remains unchanged from the last version)
     console.log("Initializing scatterplot module with config:", config);
-    
+
     fullData = config.data;
     xAccessor = config.xCol;
     yAccessor = config.yCol;
@@ -34,7 +36,7 @@ function initDashboardScatterplot(config) {
     y = d3.scaleLinear()
         .domain(d3.extent(fullData, d => +d[yAccessor])).nice()
         .range([height - marginBottom, marginTop]);
-        
+
     originalX = x.copy();
     originalY = y.copy();
 
@@ -47,11 +49,11 @@ function initDashboardScatterplot(config) {
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto;");
-        
+
     const clipId = "scatterplot-clip-path";
     svg.append("defs").append("clipPath")
         .attr("id", clipId)
-      .append("rect")
+        .append("rect")
         .attr("x", marginLeft)
         .attr("y", marginTop)
         .attr("width", width - marginLeft - marginRight)
@@ -61,20 +63,22 @@ function initDashboardScatterplot(config) {
         .attr("class", "x-axis")
         .attr("transform", `translate(0, ${height - marginBottom})`)
         .call(d3.axisBottom(x));
-    
+
     svg.append("g")
         .attr("class", "y-axis")
         .attr("transform", `translate(${marginLeft}, 0)`)
         .call(d3.axisLeft(y));
-        
+
+    // MODIFIED: Added .on("start", ...) to manage the isBrushing state
     brush = d3.brush()
         .extent([[marginLeft, marginTop], [width - marginRight, height - marginBottom]])
+        .on("start", brushStarted)
         .on("end", brushed);
 
     svg.append("g")
         .attr("class", "brush")
         .call(brush);
-    
+
     svg.append("g")
         .attr("class", "scatterplot-dots")
         .attr("clip-path", `url(#${clipId})`);
@@ -95,46 +99,62 @@ function initDashboardScatterplot(config) {
  */
 function updateDashboardScatterplot(currentYear) {
     if (!svg) { return; }
-    
+
     const chartData = fullData.filter(d => +d.Year === +currentYear);
-    
+
     const circles = svg.select(".scatterplot-dots")
-      .selectAll("circle")
-      .data(chartData, d => d.Name)
-      .join(
-          enter => enter.append("circle")
-              .attr("r", 0)
-              .attr("cx", d => x(+d[xAccessor]))
-              .attr("cy", d => y(+d[yAccessor]))
-              // Set initial opacity based on whether it's in the current view
-              .style("opacity", d => isPointInBounds(d) ? 1 : 0)
-              .call(enter => enter.transition().duration(300)
-                  .attr("r", d => radiusScale(+d[sizeAccessor]))),
-          update => update
-              .call(update => update.transition().duration(300)
-                  .attr("cx", d => x(+d[xAccessor]))
-                  .attr("cy", d => y(+d[yAccessor]))),
-          exit => exit
-              .call(exit => exit.transition().duration(300)
-                  .attr("r", 0)
-                  .style("opacity", 0)
-                  .remove())
-      );
-      
-    circles.on("mouseover", /*... event listener logic ...*/);
+        .selectAll("circle")
+        .data(chartData, d => d.Name)
+        .join(
+            enter => enter.append("circle")
+                .attr("r", 0)
+                .attr("cx", d => x(+d[xAccessor]))
+                .attr("cy", d => y(+d[yAccessor]))
+                .style("opacity", d => isPointInBounds(d) ? 1 : 0)
+                .call(enter => enter.transition().duration(300)
+                    .attr("r", d => radiusScale(+d[sizeAccessor]))),
+            update => update
+                .call(update => update.transition().duration(300)
+                    .attr("cx", d => x(+d[xAccessor]))
+                    .attr("cy", d => y(+d[yAccessor]))),
+            exit => exit
+                .call(exit => exit.transition().duration(300)
+                    .attr("r", 0)
+                    .style("opacity", 0)
+                    .remove())
+        );
+
+    // --- NEW: EVENT LISTENER LOGIC ---
+    circles
+      .on("mouseover", (event, d) => {
+          // Only show tooltip if we are NOT in the middle of a brush/zoom
+          if (isBrushing) return;
+
+          // Call your function to create the tooltip
+          createPopulationLineChart(d.Name);
+
+          // Position the tooltip near the cursor
+          d3.select(".map-tooltip")
+            .style("left", (event.pageX + 15) + "px")
+            .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", () => {
+          // Only remove tooltip if we are NOT in the middle of a brush/zoom
+          if (isBrushing) return;
+          d3.select(".map-tooltip").remove();
+      });
 }
+
 
 /**
  * (NEW HELPER) Checks if a data point is within the current domains of the x and y scales.
- * @param {object} d - The data point object.
- * @returns {boolean} - True if the point is in bounds, false otherwise.
  */
 function isPointInBounds(d) {
     const xVal = +d[xAccessor];
     const yVal = +d[yAccessor];
     const xDomain = x.domain();
     const yDomain = y.domain();
-    
+
     return xVal >= xDomain[0] && xVal <= xDomain[1] &&
            yVal >= yDomain[0] && yVal <= yDomain[1];
 }
@@ -144,29 +164,37 @@ function isPointInBounds(d) {
  * This function handles VIEW changes (i.e., after a zoom or reset).
  */
 function updateView() {
-    // Update axes
     svg.select(".x-axis").transition().duration(750).call(d3.axisBottom(x));
     svg.select(".y-axis").transition().duration(750).call(d3.axisLeft(y));
 
-    // Update circle positions AND opacity
     svg.select(".scatterplot-dots").selectAll("circle")
         .transition().duration(750)
         .attr("cx", d => x(+d[xAccessor]))
         .attr("cy", d => y(+d[yAccessor]))
-        // --- (NEW) Set opacity based on whether the point is in the new view ---
         .style("opacity", d => isPointInBounds(d) ? 1.0 : 0.1);
 }
 
+/**
+ * (NEW) This function is called when a brush gesture STARTS.
+ */
+function brushStarted() {
+    isBrushing = true;
+    // Remove any tooltip that might be active when the brush starts
+    d3.select(".map-tooltip").remove();
+}
 
 /**
  * This function is called at the end of a brush gesture.
+ * (MODIFIED to update the isBrushing state)
  */
 function brushed({selection}) {
-    // ... (This function remains unchanged from the last version)
+    // Brushing is now complete
+    isBrushing = false;
+
     if (selection) {
         const xValues = [selection[0][0], selection[1][0]].map(d => x.invert(d));
         const yValues = [selection[0][1], selection[1][1]].map(d => y.invert(d));
-        
+
         const newXDomain = d3.extent(xValues);
         const newYDomain = d3.extent(yValues);
 
